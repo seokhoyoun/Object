68095caab4061306cbf4e79c8fe7d4e7b793e9ca
Seokho Youn <axlover@hotmail.com>
2018-11-25 14:37:45 +0900

    initial commit

---
+++ src/연습용/EmpTest.java
@@ -0,0 +1,55 @@
+package 연습용;
+
+import java.util.Scanner;
+
+public class EmpTest {
+	
+
+	public static void main(String[] args) {
+		mainMenu();
+	}
+	
+	public static void mainMenu() {
+		Scanner scanner = new Scanner(System.in);
+		Employee a1 = new Employee(); int m_num;
+		do {
+			System.out.print("	******* 사원 정보 관리 프로그램 ************\r\n" + 
+					"\r\n" + 
+					"	1. 새 사원 정보 입력\r\n" + 
+					"	2. 사원 정보 수정 \r\n" + 
+					"	3. 사원 정보 삭제\r\n" + 
+					"	4. 사원정보 출력 \r\n" + 
+					"	9. 끝내기"
+					+ "메뉴 번호 입력 : ");
+					m_num = scanner.nextInt();
+			
+			switch(m_num) {
+			case 1 : a1.empInput(); break;
+			case 2 : System.out.println("새 이름을 입력하세요"); String newName = scanner.next(); a1.setName(newName); break;
+			case 3 : modifyMenu(); break;
+			case 4 : a1.empOutput(); break;
+			case 9 : break;
+			}
+		}while(m_num != 9);
+	}
+	
+	public static void modifyMenu() {
+		Employee a1 = new Employee();
+		Scanner scanner = new Scanner(System.in); int m_num;
+		System.out.print("	**** 사원 정보 수정 메뉴 *****\r\n" + 
+				"	1. 이름 변경 => setEmpName()\r\n" + 
+				"	2. 급여 변경 => setSalary()\r\n" + 
+				"	3. 부서 변경 => setDept()\r\n" + 
+				"	4. 직급 변경 => setJob()\r\n" + 
+				"	5. 이전 메뉴로 이동 => return 처리할 것");
+				m_num = scanner.nextInt();
+				
+			switch(m_num){
+			case 1 : a1.setEmpName(); break;
+			case 2 : a1.setSalary(); break;
+			case 3 : a1.setDept(); break;
+			case 4 : a1.setJob(); break;
+			case 5 : return;
+			}
+	}
+}
---
+++ src/연습용/Employee.java
@@ -0,0 +1,76 @@
+package 연습용;
+
+import java.util.Scanner;
+
+public class Employee {
+	private Scanner scanner = new Scanner(System.in);
+	private int empNo;
+	private String empName;
+	private String dept; // 소속 부서
+	private String job; // 직급
+	private int age;
+	private char gender;
+	private int salary;
+	private double bonusPoint;
+	private String phone;
+	private String address;
+	
+		public void empInput() {
+			System.out.print("사번 : ");
+			empNo = scanner.nextInt();
+			System.out.print("이름 : ");
+			empName = scanner.next();
+			System.out.print("소속부서 : ");
+			dept = scanner.next();
+			System.out.print("직급 : ");
+			job = scanner.next();
+			System.out.print("나이 : ");
+			age = scanner.nextInt();
+			System.out.print("성별(m/f) : ");
+			gender = scanner.next().toLowerCase().charAt(0);
+			System.out.print("급여 : ");
+			salary = scanner.nextInt();
+			System.out.print("보너스 포인트 : ");
+			bonusPoint = scanner.nextDouble();
+			System.out.print("핸드폰 번호 : ");
+			phone = scanner.next();
+			System.out.print("주소 : ");
+			address = scanner.nextLine();
+			address = scanner.nextLine();
+			System.out.println();
+		}
+		
+		public void empOutput() {
+			System.out.println(empNo);
+			System.out.println(empName);
+			System.out.println(dept);
+			System.out.println(job);
+			System.out.println(age);
+			System.out.println(gender);
+			System.out.println(salary);
+			System.out.println(bonusPoint);
+			System.out.println(phone);
+			System.out.println(address);
+		}
+		
+		public String setName(String newName) {//이름 변경용
+			return newName;
+			// 필드 값 변경용 메소드
+		}
+
+		public void setEmpName() {
+			
+		}
+
+		public void setSalary() {
+			
+		}
+
+		public void setDept() {
+			
+		}
+
+		public void setJob() {
+			
+		}
+}
