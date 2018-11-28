fc7f68f46ac6a030e21ff5c1b8ade8ccfd1bc5f9
Seokho Youn <axlover@hotmail.com>
2018-11-28 13:49:03 +0900

    static

---
+++ src/연습용/StaticMethod.java
@@ -0,0 +1,26 @@
+package 연습용;
+
+public class StaticMethod {
+	private static String value;
+	
+	public StaticMethod(String value) {
+		this.value = value;
+	}
+	
+	public static void setValue() {
+		
+	}
+	
+	public static String getValue(String value) {
+		
+		return value;
+	}
+	
+	public static void toUpper() {
+		System.out.println(value.toUpperCase());
+	}
+	
+	public static void setChar(String value) {
+		
+	}
+}
