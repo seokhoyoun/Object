928727199a3d05440d3a142884031cb082324f31
Seokho Youn <axlover@hotmail.com>
2018-12-28 13:44:05 +0900

    equals 오버라이딩 구현중

---
+++ src/object/method/Point.java
@@ -0,0 +1,14 @@
+package object.method;
+
+public class Point {
+	
+	private int xPos;
+	private int yPos;
+	
+	public Point(int xPos, int yPos) {
+		super();
+		this.xPos = xPos;
+		this.yPos = yPos;
+	}
+	
+}
---
+++ src/object/method/Rectangle.java
@@ -0,0 +1,21 @@
+package object.method;
+
+public class Rectangle {
+	
+	private Point upperLeft;
+	private Point lowerRight;
+	
+	public Rectangle(Point upperLeft, Point lowerRight) {
+		super();
+		this.upperLeft = upperLeft;
+		this.lowerRight = lowerRight;;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		
+	}
+	
+	
+	
+}
